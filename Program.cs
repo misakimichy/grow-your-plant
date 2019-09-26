a2cbdae004e9057107b4c521b771ebb8e334fa0c
Student <example@example.com>
2019-09-26 11:52:32 -0700

    Call UpdateState function before each turns over.

--- Program.cs
+++ Program.cs
@@ -8,37 +8,42 @@ namespace GrowPlant
 {
     class Program
     {
+        private static Plant plant = new Plant();
+        private static string userInput = "";
         public static void Main()
         {
-            InitializeGame();
+            Console.WriteLine("Let's grow your plant!");
+            Console.WriteLine("Make your plant taller and make it happier! \nOnce the water level hits more than 10 or less than 0, you plant is going to die.");
+            Console.WriteLine("\nLet's name your plant: ");
+            userInput = Console.ReadLine();
+            plant.Name = userInput;
+            RenderOption();
         }
 
-        public static void InitializeGame()
+        public static void RenderOption()
         {
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine("How do you want to take care of your plant?");
-            Console.WriteLine("1: water | 2: feed | 3: give sunshine | 4. clean weeds");
-            Console.WriteLine("I'm going to (1 - 4): ");
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine($"How do you want to take care of {plant.Name}? \n1: water | 2: feed | 3: give sunshine | 4. clean weeds");
+            Console.WriteLine("\nI'm going to (option: 1 - 4).\n");
             HandleEvent();
         }
 
         public static void HandleEvent()
         {
-            Plant plant = new Plant();
-            int userInput = int.Parse(Console.ReadLine());
+            userInput = Console.ReadLine();
 
             switch (userInput)
             {
-                case 1:
+                case "1":
                     plant.Water();
                     break;
-                case 2:
+                case "2":
                     plant.Feed();
                     break;
-                case 3:
+                case "3":
                     plant.GiveSunshine();
                     break;
-                case 4:
+                case "4":
                     plant.CleanWeeds();
                     break;
                 default:
@@ -47,20 +52,21 @@ namespace GrowPlant
                     break;
             }
             plant.RandomizeEvent();
+            CheckStatus();
         }
 
-        public static void UpdateStatus(Plant plant)
+        public static void CheckStatus()
         {
             plant.CheckAlive();
             if(plant.IsAlive)
             {
-                //DisplayStatus();
+                UpdateState();
                 RenderOption();
             }
             else
             {
                 Console.WriteLine("GAME OVER!!");
-                Console.WriteLine("Sorry, " + plant.Name + " is died...");
+                Console.WriteLine($"Sorry, {plant.Name} is died...");
                 Console.WriteLine("Play again? (Y/N)");
                 string userInput = Console.ReadLine().ToUpper();
                 if(userInput == "Y")
@@ -73,6 +79,11 @@ namespace GrowPlant
                     Console.WriteLine("See you again!");
                 }
             }
+        }
+        public static void UpdateState()
+        {
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine($"{plant.Name}'s current status: \n{plant.Name} is {plant.Height} tall. \nWater level is {plant.WaterLevel}. \nHappiness is {plant.Happiness}.\n");
         }
     }
 }
