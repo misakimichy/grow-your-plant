51c3dbfcc2082ecd3a06d477c18befa464156cd7
Student <example@example.com>
2019-09-26 09:51:07 -0700

    Add Water, Feed, GiveSunshine methods.

--- Models/GrowYourPlant.cs
+++ Models/GrowYourPlant.cs
@@ -2,19 +2,39 @@ using System;
 
 namespace GrowPlant.Models
 {
-    class Plant
+    public class Plant
     {
         public string Name { get; set; }
         public int Height { get; set;}
         public int WaterLevel { get; set; }
+        public int AttentionLevel { get; set; }
         public bool IsAlive { get; set; }
 
         public Plant (string name, int height, int waterLevel, bool isAlive)
         {
-            Name = name;
-            Height = height;
-            WaterLevel = waterLevel;
-            IsAlive = isAlive;
+            Name = "";
+            Height = 0;
+            WaterLevel = 3;
+            AttentionLevel = 0;
+            IsAlive = true;
+        }
+
+        public void Water()
+        {
+            Height += 1;
+            WaterLevel += 2;
+        }
+
+        public void Feed()
+        {
+            AttentionLevel += 1;
+        }
+
+        public void GiveSunshine()
+        {
+            Height += 1;
+            WaterLevel -= 1;
+            AttentionLevel += 2;
         }
     }
 }
